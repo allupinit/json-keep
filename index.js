5a3b979916971367ef860ebf6e5ff479d8dd7839
allupinit <keith@allupin.it>
2016-08-27 12:54:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function keepOnly(inData,keepers){ // gets rid of everything but specified keys
+  var outData = {} //set as object
+  for (var i = 0, len = keepers.length; i < len; i++) { // iterate through all keys
+    if (inData[keepers[i]]) outData[keepers[i]] = inData[keepers[i]];
+  }
+  return outData;
+}
+
+module.exports = keepOnly;
